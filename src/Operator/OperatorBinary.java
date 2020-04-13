72140adba856884ce66945639c176ba6509d2544
ariefarisi <mif160399@gmail.com>
2020-04-13 16:02:56 +0800

    Operator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -5,4 +5,14 @@ public class OperatorBinary {
     String nama = "Muhammad Irhash Farisi";
     int jumlahSks = 24;
     double indeksPresentasiKumulatif = 3.90;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+    }
 }
