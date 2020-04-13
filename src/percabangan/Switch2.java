4558553bb0a9dc6d1006bfd05861964722aea97c
ariefarisi <mif160399@gmail.com>
2020-04-13 17:10:57 +0800

    percabangan Switch 2

---
+++ src/percabangan/Switch2.java
@@ -0,0 +1,26 @@
+package percabangan;
+
+public class Switch2 {
+    public static void main(String[] args) {
+        char nilai = 'D';
+        switch (nilai){
+            case 'A':
+                System.out.println(" Abang : \"Bang, Minjam duit dong\"");
+                System.out.println("Ade :  \"Ogah\"  ");
+                break;
+            case 'B':
+                System.out.println("mhs : \"Kenapa Ulun Kd dapat A, pak? \" ");
+                System.out.println("dsn : \" !#@#\" ");
+                break;
+            case 'c':
+                System.out.println("mhs : \"Terimakasih pak \" ");
+                System.out.println("dsn : \"Selamat yaa..\"");
+                break;
+            default:
+                System.out.println("aku : \"hay, boleh minta wa lah\"");
+                System.out.println("doi : \"Gamau :p \" ");
+                break;
+
+        }
+    }
+}
